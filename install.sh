fb7f451697782bc9f78097628b849e322d97e54b
kamijodev <kamijodev@gmail.com>
2024-12-25 20:28:40 +0000

    chore

--- install.sh
+++ install.sh
@@ -14,7 +14,11 @@ sdf global rust latest
 # sheldon install
 cargo install sheldon
 
+echo "----場所の把握----"
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+echo $SCRIPT_DIR
+echo $PWD
+
 ln -sf "$SCRIPT_DIR/.gitconfig" "$HOME/.gitconfig"
 ln -sf "$SCRIPT_DIR/.zshrc" "$HOME/.zshrc"
 ln -sfn "$SCRIPT_DIR/.config" "$HOME/.config"
